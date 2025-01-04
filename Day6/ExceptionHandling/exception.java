224abb869f4e8c21fbc8e45a6e99e3a9a4f5d77e
Shan Adams M <159762368+adams045@users.noreply.github.com>
2025-01-04 09:18:01 +0530

    Update exception.java

--- Day6/ExceptionHandling/exception.java
+++ Day6/ExceptionHandling/exception.java
@@ -5,8 +5,8 @@ public class exception {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try{
-            int num1=sc.nextInt();
-            int num2=sc.nextInt();
+            int num1=Integer.parseInt(sc.nextInt());
+            int num2=Integer.parseInt(sc.nextInt());
             float value = num1/num2;
             System.out.println(value);
         }
