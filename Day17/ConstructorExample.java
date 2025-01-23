94e7c82dfb1210242d8fca4d92edac8b67326154
Shan Adams M <159762368+adams045@users.noreply.github.com>
2025-01-23 09:35:27 +0530

    Update ConstructorExample.java

--- Day17/ConstructorExample.java
+++ Day17/ConstructorExample.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 public class ConstructorExample {
     public static void main(String[] args) {
         StudentFactory stu = Student::new;
-        stu.create("vishnu",98);
+        stu.create("adams",98);
 
     }
 }
